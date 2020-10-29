9e37e92235cdfbbb013ddcac4c043f1ce9a07381
Tokur86 <tabele86@gmail.com>
2020-10-29 12:15:07 -0500

    error message object

--- controllers/register.js
+++ controllers/register.js
@@ -38,7 +38,7 @@ const handleRegister = (req, res, db, bcrypt) => {
 				.then(trx.commit)
 				.catch(trx.rollback);
 		})
-		.catch((err) => res.status(400).json(`${Error}`));
+		.catch((err) => res.status(400).alert(err.message));
 };
 
 module.exports = {
